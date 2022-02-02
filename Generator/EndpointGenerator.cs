820be1806d33ca0069f3e6672636c936b5d7b5d9
durid-ah <ahmad.durid96@gmail.com>
2022-02-01 18:05:03 -0700

    added code generation class stub

---
+++ Generator/EndpointGenerator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generator
+{
+    [Generator]
+    public class EndpointGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
