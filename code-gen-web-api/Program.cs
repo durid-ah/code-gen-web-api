8ffca19f7b6fe505796ecba8789e2939bea35b04
durid-ah <ahmad.durid96@gmail.com>
2022-02-01 17:38:36 -0700

    created the test methods and a concrete implementation of how the code should look like

--- code-gen-web-api/Program.cs
+++ code-gen-web-api/Program.cs
@@ -1,9 +1,12 @@
+using code_gen_web_api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<SomeTestMethods>();
 
 var app = builder.Build();
 
@@ -16,24 +19,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapPost("/NoParamReturnVal", (SomeTestMethods methods) => methods.NoParamReturnVal()).WithName("NoParamReturnVal");
+app.MapPost("/ParamReturnVal", (SomeTestMethods methods, string value) => methods.ParamReturnVal(value)).WithName("ParamReturnVal");
 
 app.Run();
 
---
+++ code-gen-web-api/SomeTestMethods.cs
@@ -0,0 +1,14 @@
+﻿namespace code_gen_web_api;
+
+ public class SomeTestMethods
+ {
+    public string NoParamReturnVal()
+    {
+        return "NoParamReturnVal";
+    }
+
+    public string ParamReturnVal(string value)
+    {
+        return $"ParamReturnVal {value}";
+    }
+}
