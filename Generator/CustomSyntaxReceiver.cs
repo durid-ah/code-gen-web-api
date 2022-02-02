50d4ef525c7023fdba9d34bee0a9ba1b25a69926
durid-ah <ahmad.durid96@gmail.com>
2022-02-01 19:42:37 -0700

    Added syntax receiver

---
+++ Generator/CustomSyntaxReceiver.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generator
+{
+    public class CustomSyntaxReceiver : ISyntaxReceiver
+    {
+        public ClassDeclarationSyntax ClassToAugment { get; private set; }
+        public ClassDeclarationSyntax SupplementClass { get; private set; }
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            // Business logic to decide what we're interested in goes here
+            if (syntaxNode is ClassDeclarationSyntax cds &&
+                cds.Identifier.ValueText == "EndpointBuilder")
+            {
+                ClassToAugment = cds;
+            }
+
+            // Business logic to decide what we're interested in goes here
+            if (syntaxNode is ClassDeclarationSyntax cd &&
+                cd.Identifier.ValueText == "SomeTestMethods")
+            {
+                SupplementClass = cd;
+            }
+        }
+    }
+}
